ba82495a77dbd26348400c7b4a342604f30e7fa9
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-09-17 13:04:08 +1000

    Adds Files

---
+++ Loan/src/main/java/me/sirmonkeyboy/loan/Loan.java
@@ -0,0 +1,44 @@
+package me.sirmonkeyboy.loan;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Loan extends JavaPlugin {
+
+    private static Economy econ = null;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        if (!setupEconomy() ) {
+            System.out.println("Disabled due to no Vault dependency found!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+
+        econ = rsp.getProvider();
+        //noinspection ConstantValue
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
