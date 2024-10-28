324c7ab07d618b470f5ecc35b352dd94a3d809b0
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2024-10-28 17:38:03 +1100

    1.21.1

---
+++ src/main/java/com/github/sirmonkeyboy/loan/Loan.java
@@ -0,0 +1,17 @@
+package com.github.sirmonkeyboy.loan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Loan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
