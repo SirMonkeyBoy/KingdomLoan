297c18b1452908c3aafaa105342e174d98d64fc8
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-06-06 12:12:34 +1000

    add basic logic

--- src/main/java/com/github/sirmonkeyboy/loan/Commands/LoanCommand.java
+++ src/main/java/com/github/sirmonkeyboy/loan/Commands/LoanCommand.java
@@ -26,18 +26,55 @@ public class LoanCommand implements TabExecutor {
     public boolean onCommand (@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args){
         Economy eco = Loan.getEconomy();
         if (sender instanceof Player p){
-            if (p.hasPermission("Loan.commands.loan")) {
-
-            } else {
+            if (!p.hasPermission("Loan.commands.loan")) {
                 p.sendMessage(Component.text("You Don't have permission to use /loan").color(NamedTextColor.RED));
+                return true;
+            }
+
+            if (args.length == 0) {
+                p.sendMessage(Component.text("Loan usages"));
+                p.sendMessage(Component.text("/loan create (user your loaning to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a loan.").color(NamedTextColor.GOLD)));
+                p.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan from that user 120 second time out.").color(NamedTextColor.GOLD)));
+                p.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
+                p.sendMessage(Component.text("/loan list - ").append(Component.text("Shows you the loan you have or loans have given out.").color(NamedTextColor.GOLD)));
+                return true;
+            }
+
+            String input = args[0].toLowerCase();
+
+            switch (input){
+
+                case "help":
+                    p.sendMessage(Component.text("Loan usages"));
+                    p.sendMessage(Component.text("/loan create (user your loaning to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a loan.").color(NamedTextColor.GOLD)));
+                    p.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan from that user 120 second time out.").color(NamedTextColor.GOLD)));
+                    p.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
+                    p.sendMessage(Component.text("/loan list - ").append(Component.text("Shows you the loan you have or loans have given out.").color(NamedTextColor.GOLD)));
+                    return true;
+
+                case "create":
+                    if (!p.hasPermission("Loan.commands.loan.create")) {
+                        p.sendMessage(Component.text("You Don't have permission to use /loan create").color(NamedTextColor.RED));
+                        return true;
+                    }
+                    return true;
+
+                case "accept":
+                    return true;
+
+                case "pay":
+                    return true;
+
+                case "list":
+                    return true;
+
             }
         }
         return false;
     }
 
     @Override
-    public @Nullable List<String> onTabComplete (@NotNull CommandSender commandSender, @NotNull Command
-            command, @NotNull String s, @NotNull String[]strings){
+    public @Nullable List<String> onTabComplete (@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args){
         return List.of();
     }
 }
