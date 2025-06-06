29baaa7b8dd489218ed49c62fb60ee1ef56ec047
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-06-06 15:16:14 +1000

    Create PlayerJoinListener.java

---
+++ src/main/java/com/github/sirmonkeyboy/loan/Listeners/PlayerJoinListener.java
@@ -0,0 +1,26 @@
+package com.github.sirmonkeyboy.loan.Listeners;
+
+import com.github.sirmonkeyboy.loan.Utils.MariaDB;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.sql.SQLException;
+
+
+public class PlayerJoinListener implements Listener {
+
+    private final MariaDB data;
+
+    public PlayerJoinListener(MariaDB data) {
+        this.data = data;
+    }
+
+    @EventHandler
+    public void onJoinEvent(PlayerJoinEvent event) throws SQLException {
+        Player player = event.getPlayer();
+        data.updatePlayerName(player);
+    }
+}
