38c88a0003bdc9e5417b6fe308618d60de178f73
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-06-11 19:05:38 +1000

    Refactor p to player

--- src/main/java/com/github/sirmonkeyboy/kingdomloan/Commands/LoanCommand.java
+++ src/main/java/com/github/sirmonkeyboy/kingdomloan/Commands/LoanCommand.java
@@ -35,19 +35,19 @@ public class LoanCommand implements TabExecutor {
 
     @Override
     public boolean onCommand (@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (sender instanceof Player p){
-            if (!p.hasPermission("Loan.commands.loan")) {
-                p.sendMessage(Component.text("You Don't have permission to use /loan").color(NamedTextColor.RED));
+        if (sender instanceof Player player){
+            if (!player.hasPermission("Loan.commands.loan")) {
+                player.sendMessage(Component.text("You Don't have permission to use /loan").color(NamedTextColor.RED));
                 return true;
             }
 
             if (args.length == 0) {
-                p.sendMessage(Component.text("Loan usages"));
-                p.sendMessage(Component.text("/loan create (user your lending to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a request loan.").color(NamedTextColor.GOLD)));
-                p.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan request from that user " + configManager.getRequestTimeout() + " second request timeout.").color(NamedTextColor.GOLD)));
-                p.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
-                p.sendMessage(Component.text("/loan list - ").append(Component.text("Active loans you have or have given out.").color(NamedTextColor.GOLD)));
-                p.sendMessage(Component.text("/loan history (borrowed/lent) (page) - ").append(Component.text("List of loans you have borrowed or lent up to 10 loans per page.").color(NamedTextColor.GOLD)));
+                player.sendMessage(Component.text("Loan usages"));
+                player.sendMessage(Component.text("/loan create (user your lending to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a request loan.").color(NamedTextColor.GOLD)));
+                player.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan request from that user " + configManager.getRequestTimeout() + " second request timeout.").color(NamedTextColor.GOLD)));
+                player.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
+                player.sendMessage(Component.text("/loan list - ").append(Component.text("Active loans you have or have given out.").color(NamedTextColor.GOLD)));
+                player.sendMessage(Component.text("/loan history (borrowed/lent) (page) - ").append(Component.text("List of loans you have borrowed or lent up to 10 loans per page.").color(NamedTextColor.GOLD)));
                 return true;
             }
 
@@ -56,51 +56,51 @@ public class LoanCommand implements TabExecutor {
             switch (input){
 
                 case "help":
-                    p.sendMessage(Component.text("Loan usages"));
-                    p.sendMessage(Component.text("/loan create (user your lending to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a request loan.").color(NamedTextColor.GOLD)));
-                    p.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan request from that user " + configManager.getRequestTimeout() + " second request timeout.").color(NamedTextColor.GOLD)));
-                    p.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
-                    p.sendMessage(Component.text("/loan list - ").append(Component.text("Active loans you have or have given out.").color(NamedTextColor.GOLD)));
-                    p.sendMessage(Component.text("/loan history (borrowed/lent) (page) - ").append(Component.text("List of loans you have borrowed or lent up to 10 loans per page.").color(NamedTextColor.GOLD)));
+                    player.sendMessage(Component.text("Loan usages"));
+                    player.sendMessage(Component.text("/loan create (user your lending to) (loan Amount) (pay back Amount) - ").append(Component.text("Create's a request loan.").color(NamedTextColor.GOLD)));
+                    player.sendMessage(Component.text("/loan accept (Username) - ").append(Component.text("Accepts the loan request from that user " + configManager.getRequestTimeout() + " second request timeout.").color(NamedTextColor.GOLD)));
+                    player.sendMessage(Component.text("/loan pay (Amount) - ").append(Component.text("Pays back your loan.").color(NamedTextColor.GOLD)));
+                    player.sendMessage(Component.text("/loan list - ").append(Component.text("Active loans you have or have given out.").color(NamedTextColor.GOLD)));
+                    player.sendMessage(Component.text("/loan history (borrowed/lent) (page) - ").append(Component.text("List of loans you have borrowed or lent up to 10 loans per page.").color(NamedTextColor.GOLD)));
                     return true;
 
                 case "create":
-                    if (!p.hasPermission("Loan.commands.loan.create")) {
-                        p.sendMessage(Component.text("You Don't have permission to use /loan create").color(NamedTextColor.RED));
+                    if (!player.hasPermission("Loan.commands.loan.create")) {
+                        player.sendMessage(Component.text("You Don't have permission to use /loan create").color(NamedTextColor.RED));
                         return true;
                     }
-                    loanManager.loanCreate(p, args);
+                    loanManager.loanCreate(player, args);
                     return true;
 
                 case "accept":
                     try {
-                        loanManager.loanAccept(p, args);
+                        loanManager.loanAccept(player, args);
                     } catch (SQLException e) {
-                        p.sendMessage(Component.text("Error in creating the loan try again or contact staff.").color(NamedTextColor.RED));
+                        player.sendMessage(Component.text("Error in creating the loan try again or contact staff.").color(NamedTextColor.RED));
                         return true;
                     }
                     return true;
 
                 case "pay":
                     try {
-                        loanManager.loanPay(p, args);
+                        loanManager.loanPay(player, args);
                     } catch (SQLException e) {
-                        p.sendMessage(Component.text("Error in paying down the loan try again or contact staff.").color(NamedTextColor.RED));
+                        player.sendMessage(Component.text("Error in paying down the loan try again or contact staff.").color(NamedTextColor.RED));
                         return true;
                     }
                     return true;
 
                 case "list":
                     try {
-                        loanManager.loanList(p);
+                        loanManager.loanList(player);
                     } catch (SQLException e) {
-                        p.sendMessage(Component.text("Error in getting loan list try again or contact staff.").color(NamedTextColor.RED));
+                        player.sendMessage(Component.text("Error in getting loan list try again or contact staff.").color(NamedTextColor.RED));
                         return true;
                     }
                     return true;
 
                 case "history":
-                    p.sendMessage("test");
+                    player.sendMessage("test");
                     return true;
             }
         }
@@ -147,7 +147,7 @@ public class LoanCommand implements TabExecutor {
 
         if (args[0].equalsIgnoreCase("history")) {
             if (args.length == 2) {
-                return List.of("had", "given");
+                return List.of("lent", "borrowed");
             }
         }
         return List.of();
